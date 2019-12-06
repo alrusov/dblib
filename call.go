1f581a73ec461c3f62d92f116a5daef74c8ea007
Alexey Rusov <rolic402@mail.ru>
2019-12-06 16:05:41 +0300

    +CallWithoutSecuring

--- call.go
+++ call.go
@@ -8,18 +8,30 @@ import (
 
 //----------------------------------------------------------------------------------------------------------------------------//
 
-// Call -- execute stored procedure
-func (me *DBext) Call(id uint64, name string, params ...interface{}) (_ interface{}, code int, err error) {
+func (me *DBext) call(withoutSecuring bool, id uint64, name string, params ...interface{}) (_ interface{}, code int, err error) {
 	t0 := misc.NowUTC().UnixNano()
 	defer misc.LogProcessingTime("", id, "db.call", "", t0)
 
-	_, err = me.Exec(name, params...)
+	if withoutSecuring {
+		_, err = me.ExecWithoutSecuring(name, params...)
+	} else {
+		_, err = me.Exec(name, params...)
+	}
 	if err != nil {
 		return nil, http.StatusInternalServerError, err
 	}
 
 	return nil, http.StatusOK, nil
+}
 
+// Call -- execute stored procedure
+func (me *DBext) Call(id uint64, name string, params ...interface{}) (_ interface{}, code int, err error) {
+	return me.call(false, id, name, params...)
+}
+
+// CallWithoutSecuring -- execute stored procedure
+func (me *DBext) CallWithoutSecuring(id uint64, name string, params ...interface{}) (_ interface{}, code int, err error) {
+	return me.call(true, id, name, params...)
 }
 
 //----------------------------------------------------------------------------------------------------------------------------//
